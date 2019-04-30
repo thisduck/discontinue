be54b874e0c17f6f2fb9e4ca97df10b2108696de
Adnan Ali <adnan.ali@gmail.com>
2019-04-30 03:34:52 -0400

    scroll to top on page change.

--- frontend/app/router.js
+++ frontend/app/router.js
@@ -1,7 +1,9 @@
 import EmberRouter from "@ember/routing/router";
 import config from "./config/environment";
+import RouterScroll from 'ember-router-scroll';
 
-const Router = EmberRouter.extend({
+
+const Router = EmberRouter.extend(RouterScroll, {
   location: config.locationType,
   rootURL: config.rootURL
 });
